e9ce43442abd5592ade8721290d66060e5fe217d
Dubinin Denis <d.dubinin@mfg.local>
2024-02-28 10:51:52 +0300

    ТЗ 18.2.1 Main-service

--- stat-service/stat-dto/src/main/java/ru/practicum/StatDto.java
+++ stat-service/stat-dto/src/main/java/ru/practicum/StatDto.java
@@ -1,14 +1,17 @@
 package ru.practicum;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class StatDto {
     private String app;
     private String uri;
